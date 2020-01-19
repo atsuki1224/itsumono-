c98b812ac87637f85213b3faf86a6396f01ece94
atsuki1224 <akeve1224@icloud.com>
2020-01-19 21:06:49 +0900

    groups view

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,9 @@
 class GroupsController < ApplicationController
   def index
+    @group = Group.where(params[id: current_user.id])
+  end
+
+  def show
   end
 
   def new
@@ -17,9 +21,22 @@ class GroupsController < ApplicationController
   end
 
   def edit
+    @group = Group.find(params[:id])
   end
 
   def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    group = Group.find(params[:id])
+    group.destroy
+    redirect_to root_path
   end
 
   private
