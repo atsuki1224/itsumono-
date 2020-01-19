d8060978e3eb3ddc8218dee4ad8882a4c047782b
atsuki1224 <akeve1224@icloud.com>
2020-01-20 00:00:55 +0900

    g model controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   root to: "homes#index"
   get "homes/index" => "homes#index"
   get "groups/index" => "groups#index"
-  # get "groups/:id/edit" => "groups#edit"
   delete "groups/:id" => "groups#destroy"
+  get "groups/:id/posts/index" => "posts#index"
   resources :groups
+  resources :posts
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_19_022743) do
+ActiveRecord::Schema.define(version: 2020_01_19_144215) do
 
   create_table "group_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "group_id"
@@ -28,6 +28,18 @@ ActiveRecord::Schema.define(version: 2020_01_19_022743) do
     t.index ["name"], name: "index_groups_on_name", unique: true
   end
 
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "image", null: false
+    t.text "capacity"
+    t.text "false"
+    t.text "comment"
+    t.bigint "group_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["group_id"], name: "index_posts_on_group_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -43,4 +55,5 @@ ActiveRecord::Schema.define(version: 2020_01_19_022743) do
 
   add_foreign_key "group_users", "groups"
   add_foreign_key "group_users", "users"
+  add_foreign_key "posts", "groups"
 end
