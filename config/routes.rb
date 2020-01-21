8d752ae0faffb91c6fe5590130fff90119f7178d
atsuki1224 <akeve1224@icloud.com>
2020-01-21 15:35:35 +0900

    add files

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "homes#index"
   get "homes/index" => "homes#index"
+  get "homes/how_to_use" => "homes#how_to_use"
   get "groups/index" => "groups#index"
   delete "groups/:id" => "groups#destroy"
   get "groups/:id/posts/index" => "posts#index"
